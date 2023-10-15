bf2c8a8c4bfab5c098695c6541df442c9245baa3
Samrata912 <samrata912@gmail.com>
2023-10-15 23:54:44 +0530

    Put API for updating the connection by connection id

--- src/main/java/com/velocity/Service/ConnectionListService.java
+++
@@ -1,8 +0,0 @@
-package com.velocity.Service;
-
-import com.velocity.Bean.ConnectionList;
-
-public interface ConnectionListService {
-
-    public ConnectionList updateConnectionList(ConnectionList connectionList);
-}
---
+++ src/main/java/com/velocity/network_connection/service/PutNetworkConnectionService.java
@@ -0,0 +1,8 @@
+package com.velocity.network_connection.service;
+
+import com.velocity.model.NetworkConnection;
+
+public interface PutNetworkConnectionService {
+
+    public NetworkConnection putNetworkConnection(NetworkConnection networkConnection);
+}
